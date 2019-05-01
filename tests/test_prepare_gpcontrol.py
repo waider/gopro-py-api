4cc5cefb5fdd6626e95a03b342242794c8a01496
Ronan Waide <waider@waider.ie>
2019-05-01 08:08:48 +0100

    add tests for prepare_gpcontrol

---
+++ tests/test_prepare_gpcontrol.py
@@ -0,0 +1,26 @@
+from .conftest import GoProCameraTest
+import pytest
+from socket import timeout
+
+class PrepareGpControlTest(GoProCameraTest):
+    def test_prepare_gpcontrol_success(self):
+        self.goprocam.prepare_gpcontrol()
+        # no results or side-effects by default
+
+    def test_prepare_gpcontrol_failed(self):
+        del(self.responses['/gp/gpControl'])
+        # this basically goes into infinite recursion if it fails
+        with pytest.raises(RecursionError):
+            self.goprocam.prepare_gpcontrol()
+
+    def test_prepare_gpcontrol_timeout(self):
+        self.responses['/gp/gpControl'] = timeout()
+        with pytest.raises(RecursionError):
+            self.goprocam.prepare_gpcontrol()
+
+    def test_prepare_gpcontrol_hx(self):
+        self.responses['/gp/gpControl']['info']['firmware_version'] = 'HX'
+        # HX needs an extra URL
+        # '31' > 1 means we're ready
+        self.responses['/gp/gpControl/status'] = { 'status': { '31': 2 }}
+        self.goprocam.prepare_gpcontrol()
