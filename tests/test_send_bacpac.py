977b2d92584bbcee36067f9196fb1f9aecd5665b
Ronan Waide <waider@waider.ie>
2019-05-03 23:00:29 +0100

    test sendBacpac

--- tests/test_send_bacpac.py
+++ tests/test_send_bacpac.py
@@ -7,8 +7,8 @@ from goprocam import GoProCamera
 from socket import timeout
 
 
-class SendCameraTest(GoProCameraTest):
-    def test_send_camera_default(self):
+class SendBacPacTest(GoProCameraTest):
+    def test_send_bacpac_default(self):
         with self.monkeypatch.context() as m:
             m.setattr(GoProCamera.GoPro, 'getPassword',
                       lambda self: 'password')
@@ -16,44 +16,35 @@ class SendCameraTest(GoProCameraTest):
             # this returns nothing so we need to be a bit more clever
             # to verify it
             def fake_urlopen(url, *args, **kwargs):
-                assert url == 'http://10.5.5.9/camera/foo?t=password'
+                assert url == 'http://10.5.5.9/bacpac/foo?t=password&p=%FF'
                 return io.BytesIO('{}'.encode('utf8'))
             m.setattr(urllib.request, 'urlopen', fake_urlopen)
-            self.goprocam.sendCamera('foo')
+            self.goprocam.sendBacpac('foo', 'FF')
 
-    def test_send_camera_with_value(self):
+    def test_send_bacpac_value_empty(self):
         with self.monkeypatch.context() as m:
             m.setattr(GoProCamera.GoPro, 'getPassword',
                       lambda self: 'password')
 
+            # this returns nothing so we need to be a bit more clever
+            # to verify it
             def fake_urlopen(url, *args, **kwargs):
-                assert url == 'http://10.5.5.9/camera/foo?t=password&p=bar'
+                assert url == 'http://10.5.5.9/bacpac/foo?t=password'
                 return io.BytesIO('{}'.encode('utf8'))
             m.setattr(urllib.request, 'urlopen', fake_urlopen)
-            self.goprocam.sendCamera('foo', 'bar')
+            self.goprocam.sendBacpac('foo', '')
 
-    def test_send_camera_with_hex_value(self):
-        with self.monkeypatch.context() as m:
-            m.setattr(GoProCamera.GoPro, 'getPassword',
-                      lambda self: 'password')
-
-            def fake_urlopen(url, *args, **kwargs):
-                assert url == 'http://10.5.5.9/camera/foo?t=password&p=%FF'
-                return io.BytesIO('{}'.encode('utf8'))
-            m.setattr(urllib.request, 'urlopen', fake_urlopen)
-            self.goprocam.sendCamera('foo', 'FF')
-
-    def test_send_camera_error(self):
+    def test_send_bacpac_error(self):
         # just for coverage, really. can't test as it stands.
         with self.monkeypatch.context() as m:
             m.setattr(GoProCamera.GoPro, 'getPassword',
                       lambda self: 'password')
-            self.goprocam.sendCamera('foo', 'bar')
+            self.goprocam.sendBacpac('foo', 'bar')
 
-    def test_send_camera_timeout(self):
+    def test_send_bacpac_timeout(self):
         # same
         with self.monkeypatch.context() as m:
             m.setattr(GoProCamera.GoPro, 'getPassword',
                       lambda self: 'password')
-            self.responses['/camera/foo?t=password&p=bar'] = timeout()
-            self.goprocam.sendCamera('foo', 'bar')
+            self.responses['/bacpac/foo?t=password&p=%FF'] = timeout()
+            self.goprocam.sendBacpac('foo', 'FF')
