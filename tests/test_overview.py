42279a8e42706b70cb928fce8323fe2bc8759045
Ronan Waide <waider@waider.ie>
2019-05-19 13:29:29 +0100

    test overview() [not great, but better than nothing]

---
+++ tests/test_overview.py
@@ -0,0 +1,60 @@
+from .conftest import GoProCameraTest, GoProCameraAuthTest
+from goprocam import GoProCamera
+
+
+class OverviewTest(GoProCameraTest):
+    def test_overview(self):
+        # need to hook print() for this
+        def mock_print(*args, **kwargs):
+            for arg in args:
+                assert arg in [
+                    'camera overview',
+                    'current mode: parsed',
+                    'current submode: parsed',
+                    'current video resolution: parsed',
+                    'current video framerate: parsed',
+                    'pictures taken: status',
+                    'videos taken: ', 'status',  # tsk
+                    'videos left: parsed',
+                    'pictures left: status',
+                    'battery left: parsed',
+                    'space left in sd card: parsed',
+                    'camera SSID: status',
+                    'Is Recording: parsed',
+                    'Clients connected: status',
+                    'camera model: info',
+                    'firmware version: info',
+                    'serial number: info',
+                    ]
+        with self.monkeypatch.context() as m:
+            m.setattr('builtins.print', mock_print)
+            m.setattr(GoProCamera.GoPro, 'parse_value',
+                      lambda s, p, v: 'parsed')
+            m.setattr(GoProCamera.GoPro, 'getStatus',
+                      lambda s, p, v: 'status')
+            m.setattr(GoProCamera.GoPro, 'infoCamera',
+                      lambda s, p: 'info')
+            self.goprocam.overview()
+
+
+class OverviewAuthTest(GoProCameraAuthTest):
+    def test_overview(self):
+        def mock_print(*args, **kwargs):
+            for arg in args:
+                assert arg in [
+                    'camera overview',
+                    'current mode: parsed',
+                    'current video resolution: parsed',
+                    'current photo resolution: parsed',
+                    'current timelapse interval: parsed',
+                    'current video Fov: parsed',
+                    'status lights: parsed',
+                    'recording: parsed',
+                    ]
+        with self.monkeypatch.context() as m:
+            m.setattr('builtins.print', mock_print)
+            m.setattr(GoProCamera.GoPro, 'parse_value',
+                      lambda s, p, v: 'parsed')
+            m.setattr(GoProCamera.GoPro, 'getStatus',
+                      lambda s, p: 'status')
+            self.goprocam.overview()
